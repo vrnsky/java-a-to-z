29b68d9fdc82ae33aaf2c11a20dc27343df3ff97
Egor Voronyansky <voronyansky.egor@yandex.ru>
2016-05-25 21:38:39 +0400

    New version of calculator
    New version of calculator

---
+++ src/main/java/ru/evrnsky/calculator/Calculator.java
@@ -0,0 +1,53 @@
+package ru.evrnsky.calculator;
+
+
+/**
+*  This class made arithmetic operations with double
+*
+*/
+public class Calculator
+{
+	
+	private double result;
+
+	/**
+	* Add first to double and save it to result
+	*/	
+	public void add(double first, double second)
+	{
+		this.result = first + second;
+	}
+
+	/**
+	* Substruct from first second and save it to result
+	*/
+	public void substruct(double first, double second)
+	{
+		this.result = first - second;
+	}
+
+	/*
+	* Multiply first on second and save it to result
+	*/
+	public void multiply(double first, double second)
+	{
+		this.result = first * second;
+	}
+
+	/**
+	* Divide first by second and save it to result
+	*/
+	public void div(double first, double second)
+	{
+		if(second == 0) second = 1;
+		this.result = first/second;
+	}
+
+	/*
+	* Return result of computing
+	*/
+	public double getResult()
+	{
+		return result;
+	}
+}
