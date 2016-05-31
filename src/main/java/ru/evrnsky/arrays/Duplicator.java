54b0d93674055dc6444ed8f2b4e39212a61eb98b
Egor Voronyansky <voronyansky.egor@yandex.ru>
2016-05-31 20:26:41 +0400

    Formatting code

--- src/main/java/ru/evrnsky/arrays/Duplicator.java
+++ src/main/java/ru/evrnsky/arrays/Duplicator.java
@@ -10,7 +10,7 @@ public class Duplicator
 	 * This method removed duplicates from String array
 	 * @param array for remove duplicates
 	 * @return array without duplicates
-     */
+	 */
 	public String[] removeDuplicates(String[] array)
 	{
 		sort(array);
@@ -21,12 +21,12 @@ public class Duplicator
 			return array;
 		}
 		while(nextElem < array.length){
-			if(array[nextElem].equals(array[currentElem])) {
+			
+			if(array[nextElem].equals(array[currentElem]))
 				nextElem++;
-			}
-			else{
+			
+			else
 				array[++currentElem] = array[nextElem++];
-			}
 		}
 		String[] output = new String[currentElem+1];
 		for(int k=0; k<output.length; k++){
@@ -39,7 +39,7 @@ public class Duplicator
 	/**
 	 * Sort string array
 	 * @param array - array for sorting
-     */
+	 */
 	private void sort(String[] array)
 	{
 		for(int index = 0; index < array.length; index++)
