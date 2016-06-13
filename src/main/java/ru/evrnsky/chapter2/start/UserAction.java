80e79efeb0e13f4c660bf44563c86430b395fd38
Egor Voronyansky <txn.nastya@gmail.com>
2016-06-13 20:30:02 +0300

    Interface for all user actions

---
+++ src/main/java/ru/evrnsky/chapter2/start/UserAction.java
@@ -0,0 +1,24 @@
+package ru.evrnsky.chapter2.start;
+
+/**
+	Using for determine user actions
+*/
+public interface UserAction {
+	
+	/**
+		Must return a number of operation
+		@return - a number of operation
+	*/
+	int key();
+	
+	/**
+		Execute action by calling methods from API Tracker
+	*/
+	void execute(Input input, Tracker tracker);
+	
+	/**
+		Information about action for user
+	*/
+	String info();
+
+}
