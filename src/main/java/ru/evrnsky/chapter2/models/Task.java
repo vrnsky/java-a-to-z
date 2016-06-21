90aba965f0847624acfbddf67bb8494b34749d9b
Egor Voronyansky <txn.nastya@gmail.com>
2016-06-21 10:44:15 +0400

    add test and docs

--- src/main/java/ru/evrnsky/chapter2/models/Task.java
+++ src/main/java/ru/evrnsky/chapter2/models/Task.java
@@ -1,8 +1,16 @@
 package ru.evrnsky.chapter2.models;
 
+/**
+	It is a extension of item class
+	Have all public variable and public, protected methods
+*/
 public class Task extends Item
 {
-	
+	/**
+		Create a new task
+		@param: String name - it is name of new task
+		@param: String desc - it is description of new task
+	*/
 	public Task(String name, String desc)
 	{
 		super(name, desc);
