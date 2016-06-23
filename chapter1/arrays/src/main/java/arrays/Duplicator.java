ec030c14991cafbd0d5b38e3adb85ae721697b30
Egor Voronyansky <txn.nastya@gmail.com>
2016-06-24 00:07:11 +0400

    Add arrays submodule to the chapter1 submodule

---
+++ chapter1/arrays/src/main/java/arrays/Duplicator.java
@@ -0,0 +1,73 @@
+package arrays;
+
+/**
+	This class needed for remove duplicates in string array
+	Delete is replaced string into "null"
+*/
+public class Duplicator
+{
+	/**
+	 * This method removed duplicates from String array
+	 * @param array for remove duplicates
+	 * @return array without duplicates
+	 */
+	public String[] removeDuplicates(String[] array)
+	{
+		int length = markDuplicates(array);
+		replaceNullToEnd(array);
+		String[] result = new String[length];
+
+		for(int index = 0; index < length; index++)
+			result[index] = array[index];
+
+		return result;
+
+	}
+
+	/**
+		Mark all duplicates null
+		@param: String[] array - array for marking duplicates
+		@return int length - count of unique values in array
+	*/
+	private int markDuplicates(String[] array)
+	{
+		int length = 0;
+		for(int index = 0; index < array.length; index++)
+		{
+			for(int barrier = index + 1; barrier < array.length; barrier++)
+			{
+				if(array[index] != null)
+				{
+					if(array[index].equals(array[barrier])) {
+						array[barrier] = null;
+						length++;
+					}
+				}
+			}
+		}
+		return length;
+	}
+
+	/**
+		Move all null to end of array
+		@param: String[] array - array for replacing null
+	*/
+	private void replaceNullToEnd(String[] array)
+	{
+		for(int index = 0; index < array.length; index++)
+		{
+			for(int barrier = index + 1; barrier < array.length; barrier++)
+			{
+				if(array[index] == null && array[barrier] != null)
+				{
+					String copyString = array[barrier];
+					array[index] = copyString;
+					array[barrier] = null;
+				}
+			}
+		}
+	}
+
+
+
+}
---
+++ chapter1/arrays/src/test/java/arrays/DuplicatorTest.java
@@ -0,0 +1,38 @@
+package arrays;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.core.Is.is;
+import java.util.Arrays;
+import org.junit.Test;
+import arrays.Duplicator;
+
+/**
+	Unit test for Duplicator.java
+*/
+public class DuplicatorTest
+{
+	/**
+		When pass to method for delete duplicates not null string array
+		Should return string of array without duplicates
+	*/
+	@Test
+	public void whenPassToMethodDeleteDuplicatesNotEmptyStringShouldDeleteAllDuplicatesByAssignItNull()
+	{
+		//Assign block
+		Duplicator deleteDuplicator = new Duplicator();
+		String[] values = new String[]{"A", "C", "C", "A", "B", "A"};
+		String[] expectedValues = new String[]{"A","C","B"};
+		
+		//Act block
+		String[] result = deleteDuplicator.removeDuplicates(values);
+		
+		//Action block
+		assertThat(Arrays.toString(result),is(Arrays.toString(expectedValues)));
+
+	}
+
+
+
+
+	
+}
