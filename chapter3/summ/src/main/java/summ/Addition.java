bab8cfd7134325af89a7192d85ef0e79b5b2508d
Egor Voronyansky <txn.nastya@gmail.com>
2016-07-03 19:45:25 +0400

    Summ task

---
+++ chapter3/summ/src/main/java/summ/Addition.java
@@ -0,0 +1,35 @@
+package summ;
+
+
+import start.*;
+/**
+ * Addition two integers
+ */
+public class Addition {
+
+    /**
+     * Instance of io for input and output data
+     */
+    private IO io;
+
+    /**
+     * Construct addition class by give it instance of io interface
+     * @param io - instance of IO interface
+     */
+    public Addition(IO io) {
+        this.io = io;
+    }
+
+    /**
+     * Ask user about two integers and summate it
+     * @return return sum of two integers which get from user
+     */
+    public int add() {
+        int numberOne = this.io.ask("Enter a first number: ", Integer.MIN_VALUE, Integer.MAX_VALUE);
+        int numberTwo = this.io.ask("Enter a second number: ", Integer.MIN_VALUE, Integer.MAX_VALUE);
+        int result = numberOne + numberTwo;
+        this.io.println(result);
+        return result;
+    }
+
+}
---
+++ chapter3/summ/src/test/java/summ/AdditionTest.java
@@ -0,0 +1,50 @@
+package summ;
+
+import org.junit.Test;
+import start.StubIO;
+import static org.junit.Assert.*;
+import static org.hamcrest.core.Is.is;
+
+/**
+ * Test for Addition.java
+ */
+public class AdditionTest {
+
+    /**
+     * When try add two correct numbers should sum it and
+     * show to user
+     */
+    @Test
+    public void whenTryAdditionTwoCorrectNumbersShouldSummItAndShowUser() {
+
+        //Assign block
+        String[] answer = new String[]{"1","2"};
+        StubIO stubIO = new StubIO(answer);
+        Addition addition = new Addition(stubIO);
+        int expected = 3;
+
+        //Act block
+        int actual = addition.add();
+
+        //Action block
+        assertThat(actual, is(expected));
+    }
+
+    /**
+     * When try add one correct integer and one wrong integer
+     * Should catch exception. At the real system io exception handle
+     * And user ask again about integer
+     */
+    @Test(expected = NumberFormatException.class)
+    public void whenTryAdditionOneWrongIntAndOneCorrectShouldAskUserAboutCorrectData() {
+
+        //Assign block
+        String[] answer = new String[]{"asd", "1"};
+        StubIO stubIO = new StubIO(answer);
+        Addition addition = new Addition(stubIO);
+
+        //Act & action block
+        addition.add();
+
+    }
+}
