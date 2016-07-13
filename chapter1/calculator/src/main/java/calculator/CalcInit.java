caa20316c82725a15645a0b1bf174a39f72b78cd
Egor Voronyansky <txn.nastya@gmail.com>
2016-07-13 09:30:01 +0400

    more clear code

--- chapter1/calculator/src/main/java/calculator/CalcInit.java
+++ chapter1/calculator/src/main/java/calculator/CalcInit.java
@@ -1,53 +1,64 @@
 package calculator;
 
+import start.IO;
+import start.Validator;
+
 import java.io.*;
 
 /**
  * It class interact with user by accept data,
  * call API of calculator and show to user result.
  */
-
-//TODO делегировать другому классу ввод и вывод из метода start()
 public class CalcInit {
 
+	/**
+	 * Handle input from user and show data for user.
+	 */
+	private IO io;
+
+	/**
+	 * Instance of Calculator API.
+	 */
+	private Calculator calculator;
+
+	/**
+	 * Save a reference to io object.
+	 * @param io instance of io interface.
+     */
+	public CalcInit(IO io) {
+		this.io = io;
+		this.calculator = new Calculator();
+	}
 	/**
 	 * Entry point of application.
 	 * @param args - key and values from start.
 	 * @throws Exception throw if reader from console fail.
      */
 	public static void main(String[] args) throws Exception {
-		CalcInit calcInit = new CalcInit();
+		CalcInit calcInit = new CalcInit(new Validator());
 		calcInit.start();
-		
 	}
 
 	/**
-	 * Main loop of program, it handles input and call api.
-	 * @throws Exception if reader fail.
+	 * Main loop of program.
      */
 	public void start() throws Exception {
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-		Calculator calculator = new Calculator();
 		double second, first;
-		String operand = "";
-		int answer = 0;
+		String operand;
+		String answer = "";
 
-		while(answer != -1) {
-			System.out.println("Enter first digit");
-			first = Double.parseDouble(reader.readLine());
-			System.out.println("Enter second digit");
-			second = Double.parseDouble(reader.readLine());
-			
-			System.out.println("Type operand");
-			operand = reader.readLine();
+		while(!"y".equals(answer)) {
+			first = Double.parseDouble(io.ask("Enter a first number: "));
+			second = Double.parseDouble(io.ask("Enter a second number: "));
+			operand = io.ask("Type operand: ");
+
 			if(operand.equals("+")) calculator.add(first, second);
 			else if (operand.equals("*")) calculator.multiply(first, second);
 			else if (operand.equals("/")) calculator.div(first, second);
 			else if (operand.equals("-")) calculator.deduct(first, second);
 			
-			System.out.printf("Result: %s\n", calculator.getResult());
-			System.out.println("Type any digit to continue or -1 for exit");
-			answer = Integer.parseInt(reader.readLine());
+			io.println(String.format("Result: %s\n", calculator.getResult()));
+			answer = io.ask("Exit? (y/n)");
 		}
 	}
 	
