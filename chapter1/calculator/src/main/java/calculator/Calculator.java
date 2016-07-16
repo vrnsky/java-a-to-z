9c69e0c8f389cd585dc1755092b210eeec993235
Egor Voronyansky <txn.nastya@gmail.com>
2016-07-16 17:36:13 +0400

    Now save previous result always

--- chapter1/calculator/src/main/java/calculator/Calculator.java
+++ chapter1/calculator/src/main/java/calculator/Calculator.java
@@ -1,5 +1,6 @@
 package calculator;
 
+
 /**
  * Usually calculator it may add, deduct, multiply and divide.
  * Use it for execute arithmetic operations with double numbers.
@@ -12,54 +13,83 @@ public class Calculator {
 	protected double result;
 
 	/**
-	 * Find correct arithmetic operation and execute it by invoke method of this class.
-	 * @param operation string view of math operator.
-	 * @param first value for calculate.
-	 * @param second value for calculate.
+	 * It fields hold result of previous calculation
      */
-	public void calc(String operation, double first, double second) {
-		if(operation.equals("+")) this.add(first, second);
-		else if (operation.equals("*")) this.multiply(first, second);
-		else if (operation.equals("/")) this.div(first, second);
-		else if (operation.equals("-")) this.deduct(first, second);
-	}
+	protected double prevResult;
 
 	/**
 	 * Add one first to second and write to result field.
-	 * @param first number for adding.
-	 * @param second number for adding.
+	 * @param number for adding.
      */
-	public void add(double first, double second) {
-		this.result = first + second;
+	public void add(double number) {
+		this.result = this.prevResult + number;
+		this.prevResult = this.result;
 	}
 
 	/**
 	 * Deduct second from first and write to result field.
-	 * @param first from it will deduct.
-	 * @param second it is what deduct.
+	 * @param number it number which will deduct.
      */
-	public void deduct(double first, double second) {
-		this.result = first - second;
+	public void deduct(double number) {
+		this.result = this.prevResult - number;
+		this.prevResult = this.result;
 	}
 
 	/**
-	 * Multiply first on second and write to result field.
-	 * @param first  first factor.
-	 * @param second second factor.
+	 * Multiply number on result of previous calculation and write to result field.
+	 * @param number second factor.
      */
-	public void multiply(double first, double second) {
-		this.result = first * second;
+	public void multiply(double number) {
+		this.result = this.prevResult * number;
+		this.prevResult = this.result;
 	}
 
 	/**
 	 * Divide first double by second double and write to result field.
 	 * If second double equals 0 should set it in 1 for prevent exception.
-	 * @param first dividend.
-	 * @param second divider.
+	 * @param number divisor.
+	 * @throws ArithmeticException if try div by zero.
      */
-	public void div(double first, double second) {
-		if(second == 0) second = 1;
-		this.result = first/second;
+	public void div(double number) {
+		if(number == 0) throw new ArithmeticException("Div by zero is illegal");
+		this.result = this.prevResult / number;
+		this.prevResult = this.result;
+	}
+
+	/**
+	 * Calculate a sinus.
+	 * @param number degrees.
+     */
+	public void sin(double number) {
+		this.result = Math.sin(number);
+		this.prevResult = this.result;
+	}
+
+	/**
+	 * Calculate a cosinus
+	 * @param number degrees.
+     */
+	public void cos(double number) {
+		this.result = Math.cos(number);
+		this.prevResult = this.result;
+	}
+
+	/**
+	 * Calculate decimal log of given number.
+	 * @param number for caculation.
+     */
+	public void log(double number) {
+		this.result = Math.log10(number);
+		this.prevResult = this.result;
+	}
+
+	/**
+	 * Calculate module of given number.
+	 * @param number for this will calculate module.
+     */
+	public void abs(double number) {
+		this.result = Math.abs(number);
+		this.prevResult = this.result;
 	}
 
 	/**
@@ -69,5 +99,15 @@ public class Calculator {
 	public double getResult() {
 		return result;
 	}
+
+	/**
+	 * Return result of previous calculation.
+	 * @return result previous result.
+     */
+	public double getPrevResult() {
+		return this.prevResult;
+	}
+
+
 
 }
