31ac198dac081eeb55dff6a52ecabaad2157553b
Egor Voronyansky <txn.nastya@gmail.com>
2016-07-18 01:05:06 +0400

    Save result near test

--- chapter3/find/src/test/java/find/FindByMaskTest.java
+++ chapter3/find/src/test/java/find/FindByMaskTest.java
@@ -15,30 +15,30 @@ public class FindByMaskTest {
     /**
      * From this path will start search and will save result.
      */
-    private static final String PATH = "F:/java-a-to-z/iml/move/";
+    private static final String PATH = "F:/java-a-to-z/chapter3/find/src/test/java/";
 
     /**
      * When user give correct keys should find file and save all data.
      */
     @Test
-    public void whenTrySearchFileByMaskShouldSaveResultInSpecifiedFile() {
+    public void whenTrySearchFileByMaskShouldSaveResultInSpecifiedFile() throws Exception {
 
         //Assign block
-        String[] keys = new String[]{"-d", PATH, "-n", "r*.txt", "-f", "-o", PATH + "/result//findbymask/findbymask.txt/"};
+        String[] keys = new String[]{"-d", PATH, "-n", "r*.txt", "-f", "-o", PATH + "/move/result/findbymask/findbymask.txt"};
         FindByMask findByMask = new FindByMask();
         Answerer answerer = null;
-        String[] expected = new String[] {"r*.txt was not found at F:\\java-a-to-z\\iml\\move\\result\\findbyname\\findbyname.txt",
-                                          "r*.txt was not found at F:\\java-a-to-z\\iml\\move\\test\\findbymask\\mask.txt",
-                                          "r*.txt was not found at F:\\java-a-to-z\\iml\\move\\test\\findbyname\\name.txt",
-                                          "r*.txt was found at F:\\java-a-to-z\\iml\\move\\test\\findnyregexp\\regexp.txt"
-                                         };
+        String[] expected = new String[] {
+        "r*.txt was not found at F:\\java-a-to-z\\chapter3\\find\\src\\test\\java\\find\\FindByMaskTest.java",
+        "r*.txt was not found at F:\\java-a-to-z\\chapter3\\find\\src\\test\\java\\find\\FindByNameTest.java",
+        "r*.txt was not found at F:\\java-a-to-z\\chapter3\\find\\src\\test\\java\\find\\FindByRegExpTest.java",
+        "r*.txt was not found at F:\\java-a-to-z\\chapter3\\find\\src\\test\\java\\find\\KeysValidatorTest.java",
+        "r*.txt was not found at F:\\java-a-to-z\\chapter3\\find\\src\\test\\java\\move\\result\\findbymask\\findbymask.txt",
+        "r*.txt was not found at F:\\java-a-to-z\\chapter3\\find\\src\\test\\java\\move\\test\\findbymask\\mask.txt",
+        "r*.txt was not found at F:\\java-a-to-z\\chapter3\\find\\src\\test\\java\\move\\test\\findbyname\\name.txt",
+        "r*.txt was found at F:\\java-a-to-z\\chapter3\\find\\src\\test\\java\\move\\test\\findbyregexp\\regexp.txt" };
         //Act block
         findByMask.find(keys);
-        try {
-            answerer = new Answerer(keys[6]);
-        } catch(IOException exp) {
-            System.out.println(exp.getMessage());
-        }
+        answerer = new Answerer(keys[6]);
         String[] actual = answerer.getAllStrings();
 
         //Action block
