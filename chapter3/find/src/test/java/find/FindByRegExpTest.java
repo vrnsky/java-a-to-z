a082725342e0efc441aaa9a34685ab2ebbd40a76
Egor Voronyansky <txn.nastya@gmail.com>
2016-07-18 01:06:01 +0400

    save result near test

--- chapter3/find/src/test/java/find/FindByRegExpTest.java
+++ chapter3/find/src/test/java/find/FindByRegExpTest.java
@@ -17,33 +17,31 @@ public class FindByRegExpTest {
     /**
      * At this path will be search and save result.
      */
-    private static final String PATH = "F:/java-a-to-z/iml/move/";
+    private static final String PATH = "F:/java-a-to-z/chapter3/find/src/test/java/";
 
     /**
      * Save all data find and not find.
      */
     @Test
-    public void whenTrySearchFileByRegExpShouldCheckThatWeWriteResultInFile() {
+    public void whenTrySearchFileByRegExpShouldCheckThatWeWriteResultInFile() throws Exception {
         //Assign block
-        String[] keys = new String[]{"-d", PATH, "-r", "^regexp.txt$", "-m", "-o", PATH + "/result//findbyregexp/findbyregexp.txt/"};
+        String[] keys = new String[]{"-d", PATH, "-r", "^regexp.txt$", "-m", "-o", PATH + "move/result//findbyregexp/findbyregexp.txt/"};
         Answerer answerer = null;
         FindByRegExp finder = new FindByRegExp();
         String[] expected = new String[]{
-                "^regexp.txt$ was not found at F:\\java-a-to-z\\iml\\move\\result\\findbymask\\findbymask.txt",
-                "^regexp.txt$ was not found at F:\\java-a-to-z\\iml\\move\\result\\findbyname\\findbyname.txt",
-                "^regexp.txt$ was not found at F:\\java-a-to-z\\iml\\move\\result\\findbyregexp\\findbyregexp.txt",
-                "^regexp.txt$ was not found at F:\\java-a-to-z\\iml\\move\\test\\findbymask\\mask.txt",
-                "^regexp.txt$ was not found at F:\\java-a-to-z\\iml\\move\\test\\findbyname\\name.txt",
-                "^regexp.txt$ was found at F:\\java-a-to-z\\iml\\move\\test\\findbyregexp\\regexp.txt"
+          "^regexp.txt$ was not found at F:\\java-a-to-z\\chapter3\\find\\src\\test\\java\\find\\FindByMaskTest.java",
+          "^regexp.txt$ was not found at F:\\java-a-to-z\\chapter3\\find\\src\\test\\java\\find\\FindByNameTest.java",
+          "^regexp.txt$ was not found at F:\\java-a-to-z\\chapter3\\find\\src\\test\\java\\find\\FindByRegExpTest.java",
+          "^regexp.txt$ was not found at F:\\java-a-to-z\\chapter3\\find\\src\\test\\java\\find\\KeysValidatorTest.java",
+          "^regexp.txt$ was not found at F:\\java-a-to-z\\chapter3\\find\\src\\test\\java\\move\\result\\findbyregexp\\findbyregexp.txt",
+          "^regexp.txt$ was not found at F:\\java-a-to-z\\chapter3\\find\\src\\test\\java\\move\\test\\findbymask\\mask.txt",
+          "^regexp.txt$ was not found at F:\\java-a-to-z\\chapter3\\find\\src\\test\\java\\move\\test\\findbyname\\name.txt",
+          "^regexp.txt$ was found at F:\\java-a-to-z\\chapter3\\find\\src\\test\\java\\move\\test\\findbyregexp\\regexp.txt"
         };
 
         //Action block
         finder.find(keys);
-        try {
-            answerer = new Answerer(PATH + "/result//findbyregexp/findbyregexp.txt/");
-        } catch (IOException exp) {
-            System.out.println("Result file N/A");
-        }
+        answerer = new Answerer(keys[6]);
         String[] actual = answerer.getAllStrings();
 
         //Action block
