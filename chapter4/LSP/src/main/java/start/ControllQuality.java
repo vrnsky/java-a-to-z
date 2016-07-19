d0e206fbc9e56a9c9cb6e45ea88237501097803a
Egor Voronyansky <txn.nastya@gmail.com>
2016-07-20 03:39:10 +0400

    remove old bugs, add new bugs

--- chapter4/LSP/src/main/java/start/ControllQuality.java
+++ chapter4/LSP/src/main/java/start/ControllQuality.java
@@ -41,34 +41,15 @@ public class ControllQuality {
         this.trash = new Trash();
     }
 
-    /**
-     * Calculate how per cent product is old.
-     * @param food product for calculation.
-     * @return percent of product fitness.
-     */
-    private int calculateFitness(Food food) {
-        Calendar calendar = food.getExpairDate();
-        calendar.set(Calendar.HOUR,23);
-        calendar.set(Calendar.MINUTE, 59);
-        calendar.set(Calendar.SECOND, 59);
 
-        Calendar today = new GregorianCalendar();
-        today.set(Calendar.HOUR, 23);
-        today.set(Calendar.MINUTE,59);
-        today.set(Calendar.SECOND, 59);
 
-        long productLife = (calendar.getTimeInMillis() - food.getCreateTime().getTimeInMillis()) / TIME_IN_DAY;
-        long productToday = (today.getTimeInMillis() - food.getCreateTime().getTimeInMillis()) / TIME_IN_DAY;
-        long percent = (productToday * 100) / productLife;
-        return (int)percent;
-    }
 
     /**
      * Move food to trash or shop or warehouse.
      * @param food product for moving.
      */
     public void moveFood(Food food) {
-        int percent = calculateFitness(food);
+        int percent = food.calculateFitness();
         if(percent < 25) {
             warehouse.addFood(food);
         } else if (percent > 25 && percent < 75) {
