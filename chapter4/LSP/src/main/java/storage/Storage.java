7ae72607645e7c3cd86da0fcff40218a59e5b44d
Egor Voronyansky <txn.nastya@gmail.com>
2016-07-23 19:23:55 +0400

    add support for recovery food

--- chapter4/LSP/src/main/java/storage/Storage.java
+++ chapter4/LSP/src/main/java/storage/Storage.java
@@ -1,6 +1,7 @@
 package storage;
 
 import food.Food;
+import food.ReproductFood;
 
 /**
  * Model for storage.
@@ -11,10 +12,17 @@ abstract public class Storage implements Suitable {
      * All product hold at this array.
      */
     private Food[] foods;
+
+    private ReproductFood[] reproductFoods;
     /**
      * It uses for correct adding new food.
      */
-    private int position = 0;
+    private int foodPosition = 0;
+
+    /**
+     * It uses for correct adding new reproductable food.
+     */
+    private int reproductPosition = 0;
 
     /**
      * It uses for collect data and return it to user.
@@ -27,6 +35,7 @@ abstract public class Storage implements Suitable {
      */
     public Storage(int capacity) {
         this.foods = new Food[capacity];
+        this.reproductFoods = new ReproductFood[capacity];
         buffer = new StringBuffer();
     }
 
@@ -44,27 +53,51 @@ abstract public class Storage implements Suitable {
      */
     public void addFood(Food food) {
         if(this.isSuitable(food)) {
-            this.foods[position++] = food;
+            this.foods[foodPosition++] = food;
         }
     }
 
     /**
+     * Add a new reproduct food to this storage.
+     * @param food model of reproduct food.
+     */
+    public void addFood(ReproductFood food) {
+        if(this.isSuitable(food)) {
+            this.reproductFoods[reproductPosition++] = food;
+        }
+    }
+
+
+    /**
      * Check that food suitable for storage.
      * @param food model of food which will check
-     * @return true if food suitable for this storage
+     * @return true if food suitable for this storage.
      */
     public boolean isSuitable(Food food) {
         return food.calculateFitness() < 100;
     }
 
-
-
+    /**
+     * Check that reproduct food suitable for this check
+     * @param food model of food which will check.
+     * @return true if food suitable for this storage.
+     */
+    public boolean isSuitable(ReproductFood food) { return food.calculateFitness() < 100;};
     /**
      * Collect info about product at the storage.
      */
     protected void fillInfo() {
         buffer.setLength(0);
         for(Food food : foods) {
+            if(food != null) {
+                buffer.append(food.toString());
+            }
+        }
+    }
+
+    protected void fillInfoAboutRecovery() {
+        buffer.setLength(0);
+        for(ReproductFood food : reproductFoods) {
             if(food != null) {
                 buffer.append(food.toString());
             }
