1f885ec8a287e1337e7f5dce4379e26977524e81
Egor Voronyansky <txn.nastya@gmail.com>
2016-07-31 00:36:00 +0400

    abstract storage

---
+++ chapter4/DIP/src/main/java/model/Storage.java
@@ -0,0 +1,59 @@
+package model;
+
+/**
+ * Model of storage.
+ */
+public abstract class Storage {
+
+    /**
+     * At this place hold all users.
+     */
+    protected User[] users;
+
+    /**
+     * Reference to the last not null element in array.
+     */
+    private int lastElement;
+
+    /**
+     * Create a new storage with given capacity.
+     * @param capacity size of user array.
+     */
+    public Storage(int capacity) {
+        this.users = new User[capacity];
+    }
+
+    /**
+     * Default constructor. Create storage with ten capacity.
+     */
+    public Storage() {
+        this(10);
+    }
+
+    /**
+     * Add new user to users array.
+     * @param user instance of user class.
+     */
+    protected void addUser(User user) {
+        for(int index = 0; index < this.users.length; index++) {
+            if(this.users[index] == null) {
+                user.setId(index);
+                this.users[index] = user;
+                this.lastElement = index;
+                break;
+            }
+        }
+    }
+
+    /**
+     * Return number of the last not null element in user array.
+     * @return number of the last no null element in user array.
+     */
+    protected int getIdLastElement() {
+        return this.lastElement;
+    }
+    abstract void createUser(Checker checker);
+    abstract void editUser(Checker checker);
+    abstract void removeUser(Checker checker);
+    abstract void showUsers();
+}
