a040b8b56f2709273cffeacd67fd6becaada4d8a
Egor Voronyansky <txn.nastya@gmail.com>
2016-07-31 00:37:01 +0400

    demo for show how to works together checker and storage

---
+++ chapter4/DIP/src/main/java/start/StartUI.java
@@ -0,0 +1,45 @@
+package start;
+
+import model.EditChecker;
+import model.UserStorage;
+
+/**
+ *  Demo of interact class UserStorage and Checker.
+ */
+public class StartUI {
+
+    /**
+     * Instance of IO system.
+     */
+    private IO io;
+
+    /**
+     * Create a new UI with given io system.
+     * @param io instance of io interface.
+     */
+    public StartUI(IO io) {
+        this.io = io;
+    }
+
+    /**
+     * Start app.
+     * @param args
+     */
+    public static void main(String[] args) {
+       new StartUI(new ConsoleIO()).init();
+    }
+
+    /**
+     * Init app, first add new user, second edit exist user, and remove user.
+     */
+    public void init() {
+        UserStorage userStorage = new UserStorage(this.io);
+        EditChecker checker = new EditChecker();
+        userStorage.createUser(checker);
+        userStorage.showUsers();
+        userStorage.editUser(checker);
+        userStorage.showUsers();
+        userStorage.removeUser(checker);
+        userStorage.showUsers();
+    }
+}
