a64fe53c39732c0b89d93b0a04f00aadde7f6920
Egor Voronyansky <txn.nastya@gmail.com>
2016-08-09 21:06:19 +0400

    another class for concrete user

---
+++ chapter4/DIP/src/main/java/model/StorageUser.java
@@ -0,0 +1,26 @@
+package model;
+
+/**
+ * Concrete implementation of user model
+ */
+public class StorageUser extends User {
+
+    /**
+     * Create concrete user.
+     * @param name of user.
+     * @param age of user.
+     */
+    public StorageUser(String name, int age) {
+        super(name, age);
+    }
+
+    /**
+     * Create a new concrete user.
+     * @param name of user.
+     * @param age of user.
+     * @param id for user.
+     */
+    public StorageUser(String name, int age, int id) {
+        super(name, age, id);
+    }
+}
