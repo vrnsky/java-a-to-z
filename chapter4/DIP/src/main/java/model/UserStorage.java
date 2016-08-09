644877b007f8d985ad3ac355aa8db83700f5074b
Egor Voronyansky <txn.nastya@gmail.com>
2016-08-09 21:06:49 +0400

    init check method once.

--- chapter4/DIP/src/main/java/model/UserStorage.java
+++ chapter4/DIP/src/main/java/model/UserStorage.java
@@ -13,32 +13,39 @@ public class UserStorage extends Storage {
     private IO io;
 
     /**
+     * Method for checking data.
+     */
+    private Checker checker;
+
+    /**
      * Create a new storage with given capacity and io system.
      * @param capacity size of user array.
      * @param io instance of io interface.
      */
-    public UserStorage(int capacity, IO io) {
+    public UserStorage(int capacity, IO io, Checker checker) {
         super(capacity);
         this.io = io;
+        this.checker = checker;
+    }
+
+
+    /**
+     * Create a new user storage with given io system and method of checking.
+     * @param io system for input and output.
+     * @param checker method for checking.
+     */
+    public UserStorage(IO io, Checker checker) {
+        this(10, io, checker);
     }
 
     /**
-     * Create a new user storage with given io system.
-     * @param io instance of io system.
+     * Create a new user.
      */
-    public UserStorage(IO io) {
-        this(10, io);
-    }
-
-    /**
-     * Create new user.
-     * @param checker instance of Checker interface.
-     */
-    public void createUser(Checker checker) {
+    public void createUser() {
         String name = io.ask("Enter a name of new user: ");
         int age = io.ask("Enter age of new user: ", 0, Integer.MAX_VALUE);
-        if(!checker.check(this.users, new User(name, age))) {
-            super.addUser(new User(name, age));
+        if(!this.checker.check(this.users, new StorageUser(name, age))) {
+            super.addUser(new StorageUser(name, age));
             this.io.println("User was added");
         } else {
             this.io.println("User with given name already exist");
@@ -47,17 +54,16 @@ public class UserStorage extends Storage {
 
     /**
      * Edit exist user.
-     * @param checker instance of checker interface.
      */
-    public void editUser(Checker checker) {
+    public void editUser() {
         String name = io.ask("Enter a name of user for edit:");
         int age = io.ask("Enter a age of user for edit", 0, Integer.MAX_VALUE);
         int id = io.ask("Enter a id of user for edit: ", 0, super.getIdLastElement() + 1);
-        if(checker.check(users, new User(name, age, id))) {
+        if(this.checker.check(users, new StorageUser(name, age, id))) {
             String newName = io.ask("Enter new name for user: ");
             int newId = io.ask("Enter a new id for user: ", 0, super.getIdLastElement());
             int newAge = io.ask("Enter a new age for user: ", 0, Integer.MAX_VALUE);
-            this.users[newId] = new User(newName, newAge, newId);
+            this.users[newId] = new StorageUser(newName, newAge, newId);
             this.io.println("User was edited.");
         } else {
             io.println("User with given data not exist!");
@@ -66,15 +72,14 @@ public class UserStorage extends Storage {
     }
 
     /**
-     * Remove exist user.
-     * @param checker instance of checker interface.
+     * Remove user from storage.
      */
-    public void removeUser(Checker checker) {
+    public void removeUser() {
         String name = io.ask("Enter a name of user for delete: ");
         int age = io.ask("Enter a age of user for delete: ", 0, Integer.MAX_VALUE);
         int id = io.ask("Enter a id of user for delete: ", 0, super.getIdLastElement());
 
-        if(checker.check(this.users, new User(name, age, id))) {
+        if(checker.check(this.users, new StorageUser(name, age, id))) {
             super.users[id] = null;
             io.println("User was removed.");
         } else {
