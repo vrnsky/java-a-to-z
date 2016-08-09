71ed52b4bf9d2fc49f24424ec04cc3e9e31b30cd
Egor Voronyansky <txn.nastya@gmail.com>
2016-08-09 21:05:36 +0400

    small changes about key value storage

--- chapter4/TDD/src/main/java/model/Template.java
+++ chapter4/TDD/src/main/java/model/Template.java
@@ -1,9 +1,11 @@
 package model;
 
+import java.util.Map;
+
 /**
  * All template makers must implement this interface.
  */
 public interface Template {
 
-    String generate(String string, Object[] data);
+    String generate(String string, Map<String, String> values);
 }
