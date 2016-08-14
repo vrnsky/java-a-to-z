99fdc0495200e60aea1b60c443cc069904040e5b
Egor Voronyansky <txn.nastya@gmail.com>
2016-08-14 12:01:21 +0400

    Simple calculator based on lambda

---
+++ chapter1/calculatorEight/src/main/java/model/Calculator.java
@@ -0,0 +1,19 @@
+package model;
+
+/**
+ * Simple calculator.
+ * This calculator may execute math operations such as addition, deduct, multiply and divide.
+ */
+public class Calculator {
+
+    /**
+     * Execute math operation which accept two arguments.
+     * @param operation implementation of MathOperation interface.
+     * @param a one integer for operation.
+     * @param b second integer for operation.
+     * @return result of calculation.
+     */
+    public int execute(MathOperation operation, int a, int b) {
+        return operation.calculate(a,b);
+    }
+}
