4324dd548183e30dc2c693bf258877ed4f07a250
Egor Voronyansky <txn.nastya@gmail.com>
2016-08-14 12:10:09 +0400

    without twice handling template

--- chapter4/TDD/src/main/java/model/SimpleGenerator.java
+++ chapter4/TDD/src/main/java/model/SimpleGenerator.java
@@ -1,4 +1,5 @@
 package model;
+
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -21,44 +22,38 @@ public class SimpleGenerator implements Template {
 
     /**
      * Generate string from string with key. Value take from key-value storage.
-     * @param template string with all keys.
+     * @param template   string with all keys.
      * @param dictionary all key-value pairs.
      * @return string with values without keys.
      */
     public String generate(String template, Map<String, String> dictionary) {
-        if(template == null || dictionary == null || dictionary.size() == 0 || !checkMatches(template, dictionary.size())) {
+        if (template == null || dictionary == null || dictionary.size() == 0) {
             throw new IllegalArgumentException("Bad arguments, please check arguments.");
         }
         StringBuffer buffer = new StringBuffer(template);
         Matcher matcher = PATTERN.matcher(buffer);
-        while(matcher.find()) {
-            buffer.replace(matcher.start(), matcher.end(), dictionary.get(getCleanKey(buffer.substring(matcher.start(), matcher.end()))));
+        try {
+            while (matcher.find()) {
+                buffer.replace(matcher.start(), matcher.end(), dictionary.get(getCleanKey(buffer.substring(matcher.start(), matcher.end()))));
+            }
+        } catch (NullPointerException exception) {
+            throw new IllegalArgumentException("Not found some key(s) at the dictionary");
+        }
+
+        if (buffer.indexOf("${") != -1 && buffer.indexOf("}") != -1) {
+            throw new IllegalArgumentException("You don\'t use all values from dictionary!");
         }
         return buffer.toString();
     }
 
     /**
-     * Count matches and check that matches equals for length of data array.
-     * @param template string with template.
-     * @param value size of data array.
-     * @return true if matches equals data.length, otherwise false.
-     */
-    public boolean checkMatches(String template, int value) {
-        Matcher matcher = PATTERN.matcher(template);
-        int count = 0;
-        while(matcher.find()) {
-            count++;
-        }
-        return count == value;
-    }
-
-    /**
      * Remove from string ${ and }.
+     *
      * @param key from template.
      * @return key without dollar and brackets.
      */
     public String getCleanKey(String key) {
-        return key.substring(2, key.length()-1);
+        return key.substring(2, key.length() - 1);
     }
 
 }
