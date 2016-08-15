cfe7d33c36066a25b1670f266e9cb303dc45ed9f
Egor Voronyansky <txn.nastya@gmail.com>
2016-08-15 19:03:29 +0400

    model for User and Role

---
+++ chapter5/Generics/src/main/java/service/Base.java
@@ -0,0 +1,38 @@
+package service;
+
+/**
+ * Base for all other models.
+ * @author evrnsky
+ * @version 1.0.
+ */
+public abstract class Base {
+
+    /**
+     * Unique string for each model
+     */
+    private String id;
+
+    /**
+     * Create new base with given string id.
+     * @param id string view of id.
+     */
+    public Base(String id) {
+        this.id = id;
+    }
+
+    /**
+     * Return id of current model.
+     * @return id.
+     */
+    public String getId() {
+        return this.id;
+    }
+
+    /**
+     * Set a new id.
+     * @param id new version of id.
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+}
