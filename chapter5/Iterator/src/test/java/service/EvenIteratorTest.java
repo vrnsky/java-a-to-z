48d9eaf6015d94c500f7d4d82e2ab81daecd40f1
Egor Voronyansky <txn.nastya@gmail.com>
2016-08-15 12:07:08 +0400

    add additional test for how works method next without call hasNext

--- chapter5/Iterator/src/test/java/service/EvenIteratorTest.java
+++ chapter5/Iterator/src/test/java/service/EvenIteratorTest.java
@@ -1,17 +1,25 @@
 package service;
 
 import org.junit.Test;
+
+import java.util.Arrays;
+
 import static org.junit.Assert.*;
 import static org.hamcrest.core.Is.is;
 
 /**
- * Created by Egor on 14.08.2016.
+ * Unit test for EvenIterator.java.
+ * @author evrnsky
+ * @version 1.0
  */
 public class EvenIteratorTest {
 
-
+    /**
+     * When ask iterator about has next even element when pointer at the end
+     * Should check that iterator return true.
+     */
     @Test
-    public void whenAskIteratorAboutHasNextElementsShouldCheckThatIteratorReturnTrue() {
+    public void whenAskIteratorAboutHasNextElementsShouldCheckThatIteratorReturnFalse() {
 
         //Assign block
         EvenIterator iterator = new EvenIterator(new int[]{2,10});
@@ -26,19 +34,42 @@ public class EvenIteratorTest {
         assertThat(actual, is(false));
     }
 
+    /**
+     * When try move across iterator should check that iterator give only even number.
+     */
     @Test
     public void whenTryGetNextEvenValueShouldCheckThatIteratorReturnCorrectValue() {
 
         //Assign block
-        EvenIterator iterator = new EvenIterator(new int[]{0,1,2});
+        EvenIterator iterator = new EvenIterator(new int[]{1,2,3,4,5,6,7,8});
+        int[] expected = new int[]{2,4,6,8};
+        int[] actual = new int[expected.length];
+        int index = 0;
 
         //Action block
-        int actual = 0;
         while(iterator.hasNext()) {
-            actual = (int)iterator.next();
+            actual[index++] = (int)iterator.next();
         }
 
         //Assert block
-        assertThat(actual, is(2));
+        assertThat(Arrays.toString(actual), is(Arrays.toString(expected)));
+    }
+
+    /**
+     * When try get value from iterator without call hasNext should check that value is correct.
+     */
+    @Test
+    public void whenTrySomeEvenValuesFromIteratorShouldCheckThatNextWorksAuto() {
+
+        //Assign block
+        EvenIterator iterator = new EvenIterator(new int[]{12,15,80,100});
+        int expected = 100;
+
+        //Action block
+        iterator.next();
+        iterator.next();
+        int actual = (int)iterator.next();
+
+        assertThat(actual, is(expected));
     }
 }
