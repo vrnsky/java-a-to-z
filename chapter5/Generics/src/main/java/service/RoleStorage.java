be2846bd44b88e8994c92adb999c21a4c97a12e2
Egor Voronyansky <txn.nastya@gmail.com>
2016-08-15 19:04:04 +0400

    storage only for role objects

---
+++ chapter5/Generics/src/main/java/service/RoleStorage.java
@@ -0,0 +1,11 @@
+package service;
+
+/**
+ * Model of RoleStorage. It hold only type role object.
+ */
+public class RoleStorage extends AbstractStorage<Role> {
+
+    public RoleStorage() {
+        super();
+    }
+}
