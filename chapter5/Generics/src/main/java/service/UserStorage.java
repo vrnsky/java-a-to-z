28c7204c2fc6584492a08dcf648d161c08a9068e
Egor Voronyansky <txn.nastya@gmail.com>
2016-08-15 19:05:21 +0400

    storage only for user objects

---
+++ chapter5/Generics/src/main/java/service/UserStorage.java
@@ -0,0 +1,11 @@
+package service;
+
+/**
+ * Model of user storage. Hold object only user type
+ */
+public class UserStorage extends AbstractStorage<User> {
+
+    public UserStorage() {
+        super();
+    }
+}
