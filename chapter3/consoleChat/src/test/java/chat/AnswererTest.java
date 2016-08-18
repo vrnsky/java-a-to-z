47585e82e47b5185e3edd3b2db92d81a9d5450b2
Egor <voronyansky.egor@yandex.ru>
2016-08-18 17:52:43 +0300

    now use relative path

--- chapter3/consoleChat/src/test/java/chat/AnswererTest.java
+++ chapter3/consoleChat/src/test/java/chat/AnswererTest.java
@@ -1,9 +1,17 @@
 package chat;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Optional;
 
 /**
  * Unit test for Answerer.java - it is return a random string from file.
@@ -18,16 +26,23 @@ public class AnswererTest {
     public void whenTryGetARandomStringFromAnswerFileShouldReturnAStringFromFile() {
 
         //Assign block
-        Answerer answerer = null;
+        Optional<Answerer> answerer = Optional.empty();
         String expected = "Hello!";
         try {
-            answerer = new Answerer("F:\\java-a-to-z\\iml\\reply.txt");
+            answerer = Optional.of(new Answerer(String.format("%s%s%s", FileUtils.getTempDirectoryPath(), File.separator, "reply.txt")));
+        } catch (FileNotFoundException exp) {
+            System.out.println("You must create a file at the your temp directory");
         } catch (IOException exception) {
             exception.printStackTrace();
         }
 
         //Act block
-        String actual = answerer.getRandomString();
+        String actual = "";
+        if(answerer.isPresent()) {
+            actual = answerer.get().getRandomString();
+        } else {
+            System.out.println("Something wrong, please check the files");
+        }
 
         //Action block
         assertThat(actual, is(expected));
