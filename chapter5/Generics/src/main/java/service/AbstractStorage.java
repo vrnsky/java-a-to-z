8534980e39d4db758a2941e1e940ee1082cccc23
Egor <voronyansky.egor@yandex.ru>
2016-08-18 20:25:33 +0300

    incapsulation, now withoud index and position

--- chapter5/Generics/src/main/java/service/AbstractStorage.java
+++ chapter5/Generics/src/main/java/service/AbstractStorage.java
@@ -1,11 +1,14 @@
 package service;
 
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
 /**
  * Model of storage which determine how to data hold and access to all value at the array.
  * @author evrnsky
  * @version 1.0.
  */
-public abstract class AbstractStorage<T> implements Store {
+public abstract class AbstractStorage<T extends Base> implements Store {
 
     /**
      * At this place hold all values.
@@ -35,31 +38,76 @@ public abstract class AbstractStorage<T> implements Store {
         this.values.add(value);
     }
 
-    /**
-     * Return value from simple array placed at the position.
-     * @param position pointer to data.
-     * @return value placed at the given position.
-     */
-    public T get(int position) {
-        return this.values.get(position);
-    }
 
     /**
-     * Remove value from simple array placed at the given position.
-     * @param position from this place value will delete.
+     * Return object which id is equals given id.
+     * @param id of searching item.
+     * @return item if it was find at the storage, otherwise false.
      */
-    public void remove(int position) {
-        this.values.delete(position);
-    }
-
-    /**
-     * Update value which already in simple array.
-     * @param position at this position value will be update.
-     * @param value new version of value.
-     */
-    public void update(int position, T value) {
-        this.values.update(position, value);
+    public T get(String id) {
+        Optional<T> value = this.findValueById(id);
+        if(!value.isPresent()) {
+            throw new NoSuchElementException("Element with given id not exist at the storage");
+        } else {
+            return value.get();
+        }
     }
 
 
+    /**
+     * Remove object with given id from storage.
+     * @param id of remove item.
+     */
+    public void remove(String id) {
+       Optional<T> value = this.findValueById(id);
+       if(!value.isPresent()) {
+           throw new NoSuchElementException("Element with given id not exist at the storage!");
+       } else {
+           for(int index = 0; index < this.values.size(); index++) {
+               if(value.get().equals(this.values.get(index))) {
+                   this.values.delete(index);
+                   break;
+               }
+           }
+       }
+    }
+
+
+    /**
+     * Find at the storage id with given id and set to the given value.
+     * @param id of item.
+     * @param newValue new version of item in storage.
+     */
+    public void update(String id, T newValue) {
+        Optional<T> value = this.findValueById(id);
+        if(!value.isPresent()) {
+            throw new NoSuchElementException("Given element not exist at the storage");
+        } else {
+            for(int index = 0; index < this.values.size(); index++) {
+                if(value.get().equals(this.values.get(index))) {
+                    this.values.update(index, newValue);
+                    break;
+                }
+            }
+        }
+    }
+
+    /**
+     * Find at the storage element with given id.
+     * @param id of searching item.
+     * @return instance of object which id is equals given id or Optional.empty
+     */
+    private Optional<T> findValueById(String id) {
+        Optional<T> result = Optional.empty();
+        for(int index = 0; index < this.values.size(); index++) {
+            T value = this.values.get(index);
+            if(value != null) {
+                if (value.getId().equals((id))){
+                    result = Optional.of(this.values.get(index));
+                    break;
+                }
+            }
+        }
+        return result;
+    }
 }
