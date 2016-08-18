5208f857bb9044db535d05a24b0d5125be7debdd
Egor <voronyansky.egor@yandex.ru>
2016-08-18 17:53:10 +0300

    now use relative path

--- chapter3/consoleChat/src/test/java/chat/LoggerTest.java
+++ chapter3/consoleChat/src/test/java/chat/LoggerTest.java
@@ -1,7 +1,12 @@
 package chat;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.Test;
+
+import java.io.File;
 import java.io.IOException;
+import java.util.Optional;
+
 import static org.junit.Assert.*;
 import static org.hamcrest.core.Is.is;
 
@@ -13,7 +18,7 @@ public class LoggerTest {
     /**
      * At this path exist all resources files.
      */
-    private static final String RESOURCE_PATH = "F:\\java-a-to-z\\iml\\test.txt";
+    private static final String RESOURCE_PATH = String.format("%s%s%s", FileUtils.getTempDirectoryPath(), File.separator, "text.txt");
 
     /**
      * When try write some data log should check that it is saved.
@@ -23,20 +28,26 @@ public class LoggerTest {
 
         //Assign block
         Logger logger = null;
-        Answerer answerer = null;
+        Optional<Answerer> answerer = Optional.empty();
+        String expected = "Hello!";
         try {
             logger = new Logger(RESOURCE_PATH);
 
             //Act block
             logger.log("Hello!");
-            answerer = new Answerer(RESOURCE_PATH);
-            String expected = "Hello!";
-            String actual = answerer.getRandomString();
-
-            //Action block
-            assertThat(actual, is(expected));
-        } catch(IOException exp) {
+            answerer = Optional.of(new Answerer(RESOURCE_PATH));
+        } catch (IOException exp) {
             exp.printStackTrace();
         }
+
+
+        String actual = "";
+        if(answerer.isPresent()) {
+            actual = answerer.get().getRandomString();
+        }
+
+        //Assert block
+        assertThat(actual, is(expected));
     }
 }
+
