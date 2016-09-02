62f00d13efd3a40310bbb359fb749196ed2e18b3
Egor Voronyansky <txn.nastya@gmail.com>
2016-09-02 18:13:51 +0400

    add test for standart equals and hashcode methods

--- chapter5/Map/src/main/java/model/StandardMethods.java
+++ chapter5/Map/src/main/java/model/StandardMethods.java
@@ -1,15 +1,33 @@
 package model;
 
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
- * Created by Egor on 02.09.2016.
+ * Check of putting.
  */
-public class StandartMethods {
+public class StandardMethods {
 
+    /**
+     * App start.
+     * @param args
+     */
     public static void main(String[] args) {
-
+        new StandardMethods().start();
     }
 
+    /**
+     * Main program.
+     */
     private void start() {
-        
+        User yegor = new User("Yegor", 0, new GregorianCalendar(1996, 10, 1));
+        User people = new User("Yegor", 0, new GregorianCalendar(1996, 10, 1));
+
+        Map<User, String> map = new HashMap<>();
+        map.put(yegor, "first");
+        map.put(people, "second");
+
+        System.out.println(map);
     }
 }
