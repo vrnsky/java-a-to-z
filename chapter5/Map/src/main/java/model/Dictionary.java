4ff4eb1613523a15aea713deffe89c92c527dcea
Egor Voronyansky <txn.nastya@gmail.com>
2016-09-02 23:05:11 +0400

    remove copy paste

---
+++ chapter5/Map/src/main/java/model/Dictionary.java
@@ -0,0 +1,19 @@
+package model;
+
+/**
+ * Created by Egor on 02.09.2016.
+ */
+public class Dictionary<K, V> {
+
+    public boolean insert(K key, V value) {
+        return false;
+    }
+
+    public V get(K key) {
+        return null;
+    }
+
+    public boolean delete(K key) {
+        return false;
+    }
+}
