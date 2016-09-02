71b01065423b65b9bff0dab18037ef883ed2a3d0
Egor Voronyansky <txn.nastya@gmail.com>
2016-09-02 18:13:13 +0400

    Add desc about default equals and hashCode

---
+++ chapter5/Map/src/main/java/model/StandardMethods.java
@@ -0,0 +1,15 @@
+package model;
+
+/**
+ * Created by Egor on 02.09.2016.
+ */
+public class StandartMethods {
+
+    public static void main(String[] args) {
+
+    }
+
+    private void start() {
+        
+    }
+}
