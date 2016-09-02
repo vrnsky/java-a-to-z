c0bd33ff6fd43ed3c2dccc9459ced6a0f8caf76a
Egor Voronyansky <txn.nastya@gmail.com>
2016-09-02 19:06:02 +0400

    add desc and equals and hashCode methods

--- chapter5/Map/src/main/java/model/User.java
+++ chapter5/Map/src/main/java/model/User.java
@@ -98,9 +98,32 @@ public class User {
         return birthday;
     }
 
+    @Override
+    public int hashCode() {
+        int result = name.hashCode();
+        result = 31 * result + children;
+        result = 31 * result + birthday.hashCode();
+        return result;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (children != user.children) return false;
+        if (name != null ? !name.equals(user.name) : user.name != null) return false;
+        return birthday != null ? birthday.equals(user.birthday) : user.birthday == null;
+
+    }
+
     /**
      * Update birthday of user.
      * @param birthday new version of birthday.
+
      */
     public void setBirthday(Calendar birthday) {
         this.birthday = birthday;
