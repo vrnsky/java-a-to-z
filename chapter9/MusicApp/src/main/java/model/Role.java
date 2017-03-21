38f298dd9a57510352e6dffa719d1ec2754194f7
Egor Voronyansky <txn.nastya@gmail.com>
2017-03-21 10:29:04 +0300

    override equals and hashCode methods

--- chapter9/MusicApp/src/main/java/model/Role.java
+++ chapter9/MusicApp/src/main/java/model/Role.java
@@ -15,6 +15,42 @@ public class Role {
         this.role = role;
     }
 
+    /**
+     * Check that given object equals with it.
+     * @param o object for compare.
+     * @return true if objects are equals, otherwise false.
+     */
+    @Override
+    public boolean equals(Object o) {
+        boolean equals = false;
+        if (this == o) {
+            equals = true;
+        }
+        if (!(o instanceof Role)) {
+            equals = false;
+        }
+
+        Role role1 = (Role) o;
+
+        if (id != role1.id) {
+            equals = false;
+        } else if (role.equals(role1.role)) {
+            equals = true;
+        }
+        return equals;
+    }
+
+    /**
+     * Return hash code for this.
+     * @return unique hash code for this object.
+     */
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + role.hashCode();
+        return result;
+    }
+
     public int getId() {
         return this.id;
     }
