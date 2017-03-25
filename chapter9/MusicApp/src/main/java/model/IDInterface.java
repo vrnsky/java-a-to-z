59586b04c5c3a2248246fee60ba53efa68714b9d
Egor Voronyansky <txn.nastya@gmail.com>
2017-03-25 21:37:10 +0300

    interface for avoid duplicate remove method in each dao

---
+++ chapter9/MusicApp/src/main/java/model/IDInterface.java
@@ -0,0 +1,15 @@
+package model;
+
+/**
+ * @author evrnsky
+ * @version 0.1
+ * @since 25.03.2017
+ */
+public interface IDInterface {
+
+    /**
+     * Return unique number.
+     * @return unique number per all object of given class.
+     */
+    int getId();
+}
