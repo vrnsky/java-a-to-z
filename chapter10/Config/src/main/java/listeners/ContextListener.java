25d921fbf5938b3ce3d807a81cbb4cc489311fcb
Egor Voronyansky <txn.nastya@gmail.com>
2017-03-27 01:35:02 +0400

    listener for open and close session factory

--- chapter10/Config/src/main/java/listeners/ContextListener.java
+++ chapter10/Config/src/main/java/listeners/ContextListener.java
@@ -9,15 +9,26 @@ import javax.servlet.ServletContextListener;
  * @author evrnsky
  * @version 0.1
  * @since 26.03.2017
- * //TODO when context created should create session factory and when context destroy should close session factory
+ *
+ * This context listener execute need for two reason.
+ * 1.When app context created needs to open connect to the database.
+ * 2.When app context destroyed need to close connect to the database.
  */
 public class ContextListener implements ServletContextListener {
 
+    /**
+     * Open connection with database with help DBManager class, which inside use Hibernate.
+     * @param sce instance of context event.
+     */
     @Override
     public void contextInitialized(ServletContextEvent sce) {
         DBManager.getInstance().buildSessionFactory();
     }
 
+    /**
+     * Close connection with database with help DBManager class, which inside use Hibernate.
+     * @param sce instance of context event.
+     */
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
         DBManager.getInstance().closeSessionFactory();
