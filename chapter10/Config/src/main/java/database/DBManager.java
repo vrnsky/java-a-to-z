6e6af8a6b6f7dc3a2db35cdd765b4339097f3092
Egor Voronyansky <txn.nastya@gmail.com>
2017-03-27 01:34:09 +0400

    add docs

--- chapter10/Config/src/main/java/database/DBManager.java
+++ chapter10/Config/src/main/java/database/DBManager.java
@@ -9,28 +9,64 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
  * @author evrnsky
  * @version 0.1
  * @since 26.03.2017
+ *
+ * Wrapper for work with database accross hibernate orm.
  */
 public class DBManager {
 
+    /**
+     * Self instance, it is singleton.
+     */
     private static final DBManager DB_MANAGER = new DBManager();
+
+    /**
+     * Configuration of hibernate.
+     */
     private static final StandardServiceRegistry REGISTRY = new StandardServiceRegistryBuilder().configure().build();
+
+    /**
+     * Factory of session.
+     */
     private SessionFactory factory;
 
+    /**
+     * Default constructor.
+     */
     private DBManager() {
     }
 
+    /**
+     * Return self.
+     * @return self instance.
+     */
     public static DBManager getInstance() {
         return DB_MANAGER;
     }
 
+    /**
+     * Return session factory, which needs for create session.
+     * @return session factory object.
+     */
     public SessionFactory getFactory() {
         return this.factory;
     }
 
+    /**
+     * This method calls at the time when context is init.
+     * You do not need call this method in client code.
+     * It call automatically by container.
+     * Init session factory object.
+     */
     public void buildSessionFactory() {
         this.factory = new MetadataSources(REGISTRY).buildMetadata().buildSessionFactory();
     }
 
+    /**
+     * This method calls at the when context is destroy.
+     * You don not need call this method in client code.
+     * It call automatically by container.
+     * Close sesssion factory.
+     */
     public void closeSessionFactory() {
         this.factory.close();
     }
