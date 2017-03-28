9e9a7594fb236825c69c4b171641aee11e34bcd1
Egor Voronyansky <txn.nastya@gmail.com>
2017-03-29 02:04:12 +0400

    filter for log in system

---
+++ chapter9/MusicApp/src/main/java/auth/LoginFilter.java
@@ -0,0 +1,62 @@
+package auth;
+
+import model.User;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+/**
+ * @author evrnsky
+ * @version 0.1
+ * @since 28.03.2017
+ */
+@WebFilter(urlPatterns = "*")
+public class LoginFilter implements Filter {
+
+    /**
+     * Call once at the time and set internal settings.
+     * @param filterConfig instance of filter config.
+     * @throws ServletException if request could not be handled.
+     */
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    /**
+     * Main method of filter.
+     * @param servletRequest from client to server.
+     * @param servletResponse from server to client.
+     * @param  chain of filter, must need throw request further to the filter chain.
+     * @throws IOException if an input or output error detected.
+     * @throws ServletException if request could not be handled.
+     */
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest)servletRequest;
+        HttpServletResponse response = (HttpServletResponse)servletResponse;
+
+        if (request.getRequestURI().contains("/login")) {
+           chain.doFilter(servletRequest, servletResponse);
+        } else {
+            HttpSession session = request.getSession();
+            User user = (User)session.getAttribute("user");
+            if (user == null) {
+                response.sendRedirect(String.format("%s/login.html", request.getContextPath()));
+                return;
+            }
+            chain.doFilter(servletRequest, servletResponse);
+        }
+    }
+
+    /**
+     * Call once at the all time.
+     * Release all resources.
+     */
+    @Override
+    public void destroy() {
+    }
+}
