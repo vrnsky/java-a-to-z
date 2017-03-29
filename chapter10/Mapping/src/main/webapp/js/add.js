574fff8ed51e98f629a73f6a1a6caccdb1e8587c
Egor Voronyansky <txn.nastya@gmail.com>
2017-03-29 14:06:29 +0400

    add stub for coming soon function

---
+++ chapter10/Mapping/src/main/webapp/js/add.js
@@ -0,0 +1,21 @@
+
+window.onload = updateForm();
+
+function updateForm() {
+    /**
+     * First query ask about producers name
+     * Second query ask about model depend on producer.
+     * And third query ask about body of car.
+     */
+}
+
+function updateCarModel(producer) {
+}
+
+function updateBodies() {
+
+}
+
+function submitForm() {
+
+}
