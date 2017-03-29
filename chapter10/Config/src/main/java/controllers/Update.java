39c548c130140714b634c399708d2b1be319d7c8
Egor Voronyansky <txn.nastya@gmail.com>
2017-03-30 01:32:25 +0400

    remove redudant imports

--- chapter10/Config/src/main/java/controllers/Update.java
+++ chapter10/Config/src/main/java/controllers/Update.java
@@ -1,6 +1,5 @@
 package controllers;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import model.Item;
 import repository.ItemRepository;
 import javax.servlet.ServletException;
@@ -9,7 +8,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 /**
  * @author evrnsky
