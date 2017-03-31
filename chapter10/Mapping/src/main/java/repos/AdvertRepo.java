7a0cd1a3b69aedb15bf8627c91ed987092991e6f
Egor Voronyansky <vrnsky@protonmail.ch>
2017-04-01 01:09:37 +0400

    implement basic functions of both repo

--- chapter10/Mapping/src/main/java/repos/AdvertRepo.java
+++ chapter10/Mapping/src/main/java/repos/AdvertRepo.java
@@ -1,10 +1,119 @@
 package repos;
 
+import database.DBManager;
+import model.Advert;
+import org.hibernate.Session;
+import java.util.List;
+
 /**
  * @author evrnsky(vrnsky at protonmail.ch)
  * @version 0.1
  * @since 29.03.2017
  */
 public class AdvertRepo {
+
+    /**
+     * It is singleton.
+     */
+    private static final AdvertRepo REPO = new AdvertRepo();
+
+    /**
+     * Wrapper for work with database.
+     */
+    private DBManager dbManager;
+
+    /**
+     * It is private, because singleton.
+     */
+    private AdvertRepo() {
+        this.dbManager = DBManager.getInstance();
+    }
+
+    /**
+     * Return instance of it.
+     * @return it.
+     */
+    public static AdvertRepo getInstance() {
+        return REPO;
+    }
+
+    /**
+     * Add new advert to the system.
+     * @param advert instance of advert class.
+     */
+    public void add(Advert advert) {
+        Session session = this.dbManager.getFactory().openSession();
+        session.beginTransaction();
+        session.save(advert);
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    /**
+     * Edit already exist at the database advert.
+     * @param advert instance of advert class.
+     */
+    public void edit(Advert advert) {
+        Session session = this.dbManager.getFactory().openSession();
+        session.beginTransaction();
+        session.update(advert);
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    /**
+     * Remove advert from database.
+     * @param advert instance of advert class.
+     */
+    public void remove(Advert advert) {
+        Session session = this.dbManager.getFactory().openSession();
+        session.beginTransaction();
+        session.remove(advert);
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    /**
+     * Return advert with given id.
+     * @param id unique among all adverts number.
+     * @return advert with given id.
+     */
+    public Advert getById(int id) {
+        Advert advert = null;
+        Session session = this.dbManager.getFactory().openSession();
+        session.beginTransaction();
+        advert = session.get(Advert.class, id);
+        session.getTransaction().commit();
+        return advert;
+    }
+
+    /**
+     * Return adverts which written by user specify by id.
+     * @param id unique number per user among all users.
+     * @return list of adverts which created user with given id.
+     */
+    public List<Advert> getAdvertsByUserId(int id) {
+        List<Advert> adverts;
+        Session session = this.dbManager.getFactory().openSession();
+        session.beginTransaction();
+        adverts = session.createQuery(String.format("from model.Advert as ad where ad.user_id = %s", id)).list();
+        session.getTransaction().commit();
+        session.close();
+        return adverts;
+    }
+
+    /**
+     * Return list of not sale adverts.
+     * @return list of not sale adverts at the system.
+     */
+    public List<Advert> getAll() {
+        List<Advert> adverts;
+        Session session = this.dbManager.getFactory().openSession();
+        session.beginTransaction();
+        adverts = session.createQuery("from model.Advert as a where a.sale = false").list();
+        session.getTransaction().commit();
+        session.close();
+        return adverts;
+    }
 }
 
--- chapter10/Mapping/src/main/java/repos/CarRepo.java
+++ chapter10/Mapping/src/main/java/repos/CarRepo.java
@@ -4,40 +4,76 @@ import database.DBManager;
 import model.Car;
 import model.CarInfo;
 import org.hibernate.Session;
-
-import java.util.ArrayList;
 import java.util.List;
 
 /**
  * @author evrnsky(vrnsky at protonmail.ch)
  * @version 0.1
  * @since 29.03.2017
+ *
+ * This is car repository.
  */
 public class CarRepo {
 
+    /**
+     * Self instance, it is singleton.
+     */
     private static final CarRepo REPO = new CarRepo();
+
+    /**
+     * Wrapper for work with database.
+     */
     private DBManager dbManager;
+
+    /**
+     * It is private, because it is singleton.
+     */
     private CarRepo() {
         this.dbManager = DBManager.getInstance();
     }
+
+    /**
+     * Return self instance.
+     * @return instance of this.
+     */
     public static CarRepo getInstance() {
         return REPO;
     }
 
+    /**
+     * Find car by given id.
+     * @param id unique among all cars in system number.
+     * @return car which id is equals to given id.
+     */
     public Car getById(int id) {
-        return null;
+        Car car = null;
+        Session session = this.dbManager.getFactory().openSession();
+        session.beginTransaction();
+        car = session.get(Car.class, id);
+        session.getTransaction().commit();
+        session.close();
+        return car;
     }
 
+    /**
+     * Return models by producer id.
+     * @param producerId unique among all producers at the system number.
+     * @return list of models.
+     */
     public List<CarInfo> getModelsByProducer(String producerId) {
         List<CarInfo> models;
         Session session = this.dbManager.getFactory().openSession();
         session.beginTransaction();
-        models = session.createQuery("from model.Model as m where producer_id = " + producerId).list();
+        models = session.createQuery(String.format("from model.Model as m where producer_id = %s", producerId)).list();
         session.getTransaction().commit();
         session.close();
         return models;
     }
 
+    /**
+     * Return list of all producers.
+     * @return list of all producers.
+     */
     public List<CarInfo> getAllProducers() {
         List<CarInfo> producers;
         Session session = this.dbManager.getFactory().openSession();
@@ -48,6 +84,10 @@ public class CarRepo {
         return producers;
     }
 
+    /**
+     * Return all bodies.
+     * @return all bodies.
+     */
     public List<CarInfo> getAllBodies() {
         List<CarInfo> bodies;
         Session session = this.dbManager.getFactory().openSession();
@@ -56,6 +96,24 @@ public class CarRepo {
         session.getTransaction().commit();
         session.close();
         return bodies;
+    }
+
+    /**
+     * Return car with given parameters.
+     * @param modelId specify model of car.
+     * @param producerId specify producer of car.
+     * @param bodyId specify body of car.
+     * @return car which have given params.
+     */
+    public Car getCarByParam(int modelId, int producerId, int bodyId) {
+        Car cars;
+        Session session = this.dbManager.getFactory().openSession();
+        session.beginTransaction();
+        cars = (Car) session.createQuery(String.format(
+                "from model.Car as c where c.model_id = %s and c.producer_id = %s and c.body_id = %s", modelId, producerId, bodyId)).getSingleResult();
+        session.getTransaction().commit();
+        session.close();
+        return cars;
     }
 
 }
