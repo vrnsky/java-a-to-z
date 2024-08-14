091a3fcdff094481e30f9a357fdd4aaaafbd5d55
Egro Voronianskii <vrnsky@protonmail.ch>
2024-08-14 13:30:39 +0800

    feat: JAVAZ-3. Обновление версии зависимостей

--- chapter10/Annotations/src/main/java/model/User.java
+++ chapter10/Annotations/src/main/java/model/User.java
@@ -1,11 +1,12 @@
 package model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Column;
-import javax.persistence.Table;
-import javax.persistence.Id;
-import javax.persistence.GenerationType;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 /**
  * @author evrnsky
